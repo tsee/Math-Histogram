1036258f61596c46d8f4c78f33f57946c9d25102
Steffen Mueller <smueller@cpan.org>
2012-02-22 08:39:23 +0100

    Test with multiple no. of bins per dimension

--- ctest/200histogram.c
+++ ctest/200histogram.c
@@ -7,28 +7,31 @@
 
 
 void test_flat_bin_number();
-mh_histogram_t *make_cubything_hist(unsigned int ndim, int varbins);
+mh_histogram_t *make_cubything_hist(unsigned int ndim, unsigned int base_nbins, int varbins);
 mh_histogram_t *histogram_clone_dance(mh_histogram_t *input);
 
 int
 main (int argc, char **argv)
 {
+  unsigned int i, j;
   UNUSED(argc);
   UNUSED(argv);
   pass();
 
-  /* without (0) and with (1) cloning */
-  test_flat_bin_number(0, MH_AXIS_OPT_FIXEDBINS);
-  test_flat_bin_number(1, MH_AXIS_OPT_FIXEDBINS);
-  test_flat_bin_number(0, MH_AXIS_OPT_VARBINS);
-  test_flat_bin_number(1, MH_AXIS_OPT_VARBINS);
+  for (i = 0; i <= 1; ++i) {
+    for (j = 2; j < 5; ++j) {
+      /* without (0) and with (1) cloning */
+      test_flat_bin_number(i, MH_AXIS_OPT_FIXEDBINS, j);
+      test_flat_bin_number(i, MH_AXIS_OPT_VARBINS, j);
+    }
+  }
 
   done_testing();
   return 0;
 }
 
 
-void test_flat_bin_number(int do_clone, int varbins)
+void test_flat_bin_number(int do_clone, int varbins, unsigned int base_nbins)
 {
   unsigned int i, x, y, z, prev_bin_no;
   mh_histogram_t *h1;
@@ -44,12 +47,12 @@ void test_flat_bin_number(int do_clone, int varbins)
   char buf[2048];
   char dimbuf[2048];
 
-  h1 = make_cubything_hist(1, varbins);
+  h1 = make_cubything_hist(1, base_nbins, varbins);
   if (do_clone != 0)
     h1 = histogram_clone_dance(h1);
-  for (i = 0; i <= 3; ++i) {
+  for (i = 0; i <= base_nbins+1; ++i) {
     dim_bins1[0] = i;
-    coord1[0] = (double)i/2.- 1e-5;
+    coord1[0] = (double)i/(double)base_nbins - 1e-5;
     is_int_m(mh_hist_flat_bin_number(h1, dim_bins1), i, "1d cubything");
     is_int_m(mh_hist_find_bin(h1, coord1), i, "1d cubything, from coord");
     mh_hist_find_bin_numbers(h1, coord1, dim_bins_buf);
@@ -57,21 +60,21 @@ void test_flat_bin_number(int do_clone, int varbins)
     is_int_m(mh_hist_find_bin_buf(h1, coord1, dim_bins_buf), i, "1d cubything, from coord buf");
   }
   
-  h2 = make_cubything_hist(2, varbins);
+  h2 = make_cubything_hist(2, base_nbins, varbins);
   if (do_clone != 0)
     h2 = histogram_clone_dance(h2);
   sprintf(dimbuf, "with o/u: xbins=%u, ybins=%u", 2+MH_AXIS_NBINS(h2->axises[0]), 2+MH_AXIS_NBINS(h2->axises[1]));
   prev_bin_no = 0;
-  for (y = 0; y <= 4; ++y) {
+  for (y = 0; y <= base_nbins+2; ++y) {
     dim_bins2[1] = y;
-    for (x = 0; x <= 3; ++x) {
-      const unsigned int exp = x + y*(2+2);
+    for (x = 0; x <= base_nbins+1; ++x) {
+      const unsigned int exp = x + y*(base_nbins+2);
       if (x != 0 || y != 0)
         is_int_m(exp, prev_bin_no+1, "contiguous bins");
       prev_bin_no = exp;
       dim_bins2[0] = x;
-      coord2[0] = (double)x/2. - 1e-5;
-      coord2[1] = (double)y/3. - 1e-5;
+      coord2[0] = (double)x/(double)base_nbins - 1e-5;
+      coord2[1] = (double)y/(double)(base_nbins+1.) - 1e-5;
       sprintf(buf, "2d cubything, x=%u y=%u, res=%u exp=%u, (%s)", x, y, mh_hist_flat_bin_number(h2, dim_bins2), exp, dimbuf);
       is_int_m(mh_hist_flat_bin_number(h2, dim_bins2), exp, buf);
       is_int_m(mh_hist_find_bin(h2, coord2), exp, buf);
@@ -82,23 +85,23 @@ void test_flat_bin_number(int do_clone, int varbins)
     }
   }
 
-  h3 = make_cubything_hist(3, varbins);
+  h3 = make_cubything_hist(3, base_nbins, varbins);
   if (do_clone != 0)
     h3 = histogram_clone_dance(h3);
   sprintf(dimbuf, "with o/u: xbins=%u, ybins=%u, zbins=%u", 2+MH_AXIS_NBINS(h3->axises[0]), 2+MH_AXIS_NBINS(h3->axises[1]), 2+MH_AXIS_NBINS(h3->axises[2]));
-  for (z = 0; z <= 5; ++z) {
+  for (z = 0; z <= base_nbins + 3; ++z) {
     dim_bins3[2] = z;
-    for (y = 0; y <= 4; ++y) {
+    for (y = 0; y <= base_nbins + 2; ++y) {
       dim_bins3[1] = y;
-      for (x = 0; x <= 3; ++x) {
-        const unsigned int exp = x + y*(2+2) + z*(3+2)*(2+2);
+      for (x = 0; x <= base_nbins + 1; ++x) {
+        const unsigned int exp = x + y*(base_nbins+2) + z*(base_nbins+1+2)*(base_nbins+2);
         if (x != 0 || y != 0 || z != 0)
           is_int_m(exp, prev_bin_no+1, "contiguous bins");
         prev_bin_no = exp;
         dim_bins3[0] = x;
-        coord3[0] = (double)x/2. - 1e-5;
-        coord3[1] = (double)y/3. - 1e-5;
-        coord3[2] = (double)z/4. - 1e-5;
+        coord3[0] = (double)x/(double)base_nbins - 1e-5;
+        coord3[1] = (double)y/(double)(base_nbins+1) - 1e-5;
+        coord3[2] = (double)z/(double)(base_nbins+2) - 1e-5;
         sprintf(buf, "3d cubything, x=%u y=%u z=%u, res=%u exp=%u, (%s)", x, y, z, mh_hist_flat_bin_number(h3, dim_bins3), exp, dimbuf);
         is_int_m(mh_hist_flat_bin_number(h3, dim_bins3), exp, buf);
 
@@ -130,14 +133,13 @@ histogram_clone_dance(mh_histogram_t *input)
 
 
 mh_histogram_t *
-make_cubything_hist(unsigned int ndim, int varbins)
+make_cubything_hist(unsigned int ndim, unsigned int base_nbins, int varbins)
 {
   unsigned int i, j;
   mh_histogram_t *h;
-  unsigned int nbins = 2;
   mh_axis_t **axises = malloc(ndim * sizeof(mh_axis_t *));
   for (i = 0; i < ndim; ++i) {
-    axises[i] = mh_axis_create(nbins+i, varbins);
+    axises[i] = mh_axis_create(base_nbins+i, varbins);
     if (axises[i] == NULL) {
       fail("Failed to malloc axis!");
       return NULL;
@@ -145,8 +147,8 @@ make_cubything_hist(unsigned int ndim, int varbins)
     mh_axis_init(axises[i], 0., 1.);
     if (varbins == MH_AXIS_OPT_VARBINS) {
       double *b = axises[i]->bins;
-      for (j = 0; j <= nbins+i; ++j) {
-        b[j] = 0. + (double)j/(nbins+i);
+      for (j = 0; j <= base_nbins+i; ++j) {
+        b[j] = 0. + (double)j/(base_nbins+i);
         /* printf("  i = %u   j = %u   =>   %f\n", i, j, b[j]); */
       }
     }
