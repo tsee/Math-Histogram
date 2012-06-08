a6f38609b8fa054eab256e0707c37600a7bf86fd
Steffen Mueller <smueller@cpan.org>
2012-06-08 18:28:49 +0200

    Documentation for Axis

--- lib/Math/Histogram/Axis.pm
+++ lib/Math/Histogram/Axis.pm
@@ -31,6 +31,51 @@ the one for determining the bin number for a given coordinate,
 will be O(1) for fixed-width binning, but O(log(n)) for variable-width
 binning.
 
+=head1 METHODS
+
+=head2 new
+
+The constructor takes one or three arguments. With three arguments, the
+axis will have fixed-width bins. The first argument is the number of
+bins on the axis, the second is the lower boundary of the first bin,
+the third is the upper boundary of the last bin.
+
+One argument indicates a variable-width binning. The argument must
+be a reference to an array of floating point numbers that increase
+strictly monotonically. They are interpreted in order
+as the lower boundary of the first bin, the boundary between first
+and second bin, ... and finally the upper boundary of the last bin.
+Given N elements of the array reference, the axis will have N-1 bins.
+
+Bin numbers start at FIXME
+
+=head2 clone
+
+Returns a deep clone of the axis object.
+
+=head2 nbins
+
+Returns the number of bins on the axis.
+
+=head2 min
+
+Returns the lower boundary of the first bin of the axis.
+
+=head2 max
+
+Returns the upper boundary of the last bin of the axis.
+
+=head2 width
+
+Returns the total width of all bins combined. Yes,
+that's simply C<$axis-E<gt>max - $axis-E<gt>min>.
+
+=head2 binsize
+
+Given a bin number, returns the width of that bin. Defaults to '1',
+so for axis objects with fixed-width binning, calling this method without
+argument is valid.
+
 =head1 SEE ALSO
 
 L<Math::Histogram>
